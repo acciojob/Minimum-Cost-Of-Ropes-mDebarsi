a10455a613d3f82636090d88844230ed1fde2548
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-11-03 15:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function calculateMinCost() {
 	let finalValue = 0;
 	while (arr.length > 1) {
 		arr.sort((a,b) =>{return(a-b)});
-		let value = parseInt (arr .shift());
+		let value = parseInt (arr.shift());
 		let value1 = parseInt(arr.shift());
 		let mainValue = value+value1;
 		finalValue = finalValue + mainValue;
